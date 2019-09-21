750dcd74469bae29ef17cb488b526f2c3971bd27
gczajka <gczajkajv@gmail.com>
2019-09-21 19:10:00 +0200

    zadanie 19.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -20,4 +20,12 @@ public class DbService {
     public Optional<Task> getTask(Long id) {
         return repository.findById(id);
     }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        repository.deleteById(id);
+    }
 }
